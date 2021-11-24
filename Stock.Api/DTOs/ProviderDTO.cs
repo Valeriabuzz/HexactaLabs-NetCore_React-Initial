b752591e195c0cb94cd73d1351e56c075b550e3a
Valeriabuzz <valeriabuzz@gmail.com>
2021-11-24 17:38:36 -0300

    Cambios Nivel Inicial _ Valeria Buzzurro

---
+++ Stock.Api/DTOs/ProviderDTO.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Stock.Api.DTOs
+{
+    public class ProviderDTO
+    {
+        [Required]
+        public string Name { get; set; }
+
+        public string Id { get; set; }
+
+        public string phone { get; set; }
+
+        public string email { get; set; }
+
+
+
+      
+
+    }
+}
---
+++ Stock.AppService/Services/ProviderService.cs
@@ -0,0 +1,21 @@
+﻿using Stock.AppService.Base;
+using Stock.Model.Entities;
+using Stock.Repository.LiteDb.Interface;
+
+namespace Stock.AppService.Services
+{
+    /// <summary>
+    /// Product type service.
+    /// </summary>
+    public class ProviderService: BaseService<Provider>
+    {                
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ProductTypeService"/> class.
+        /// </summary>
+        /// <param name="repository">Product type repository.</param>
+        public ProviderService(IRepository<Provider> repository)
+            : base(repository)
+        {
+        }
+    }
+}
